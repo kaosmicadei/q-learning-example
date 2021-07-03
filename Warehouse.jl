3a2492ae4cfceb3bceda1cfd82cc7e3da283ff31
Kaonan Micadei <k.micadei@gmail.com>
2021-07-04 01:01:40 +0200

    Define separeted types for eviroment and agent

--- Warehouse.jl
+++ Warehouse.jl
@@ -1,92 +1,92 @@
 using DelimitedFiles
+using Match
 
 # Generic abstraction of a state
 State = CartesianIndex
 
-# Possible actions the agent can execute.
-@enum Action up=1 right down left
+# Actions are encoded as index
+Action = Int
 
-struct QLearning
-  rewards::Matrix{Float64}
-  values::Array{Float64,3}
+# Define an environment
+struct Environment
+    rewards::Array{Int, N} where N
+    actions::Int
 end
 
-QLearning(rewards) = let
-  (rows, cols) = size(rewards)
-  values = zeros(Float64, rows, cols, Action.size)
-  QLearning(rewards, values)
+struct Agent
+    qvalues::Array{Float32, N} where N
+end
+Agent(e::Environment) = let dims = (size(e.rewards)..., e.actions)
+    Agent(zeros(Float32, dims))
 end
 
-function update_qvalues!(q::QLearning, state::State, nextState::State, action::Action; α=0.9, γ=0.9)
-  # α : learning rate
-  # γ : discount factor
-  action_index = Int(action)
-  reward = q.rewards[nextState]
-  oldValue = q.values[state, action_index]
-  optimalNextAction = maximum(q.values[nextState,:])
-  newValue = reward + γ * optimalNextAction
-  q.values[state, action_index] = (1-α) * oldValue + α * newValue
+function update_qvalues!(a::Agent, e::Environment, state::State, next_state::State, action::Action; α=0.9, γ=0.9)
+    # α : learning rate
+    # γ : discount factor
+    reward = e.rewards[next_state]
+    oldvalue = a.qvalues[state, action]
+    optimal_action = maximum(a.qvalues[next_state,:])
+    newvalue = reward + γ * optimal_action
+    a.qvalues[state, action] = (1-α) * oldvalue + α * newvalue
 end
 
-# Determine if the current state is terminal or if the agent can keep moving
-isterminal(position::State, q::QLearning) = q.rewards[position] != -1.0
+# # Determine if the current state is terminal or if the agent can keep moving
+isterminal(state::State, e::Environment) = e.rewards[state] != -1.0
 
-# Starting from a random location
-getStartingLocation(q::QLearning) = let validPoints = findall(s -> s == -1, q.rewards)
-  rand(validPoints)
+# # Starting from a random state
+get_starting_state(e::Environment) = findall(s -> s == -1, e.rewards) |> rand
+
+get_next_action(a::Agent, e::Environment, state::State, ϵ) = rand() < ϵ ? argmax(a.qvalues[state,:]) : rand(1:e.actions)
+
+function get_next_state(e::Environment, state::State, action::Action)
+    move = @match action begin
+        1 => State(-1,0)    # up
+        2 => State(0,1)     # right
+        3 => State(1,0)     # down
+        4 => State(0,-1)    # left
+    end
+    next = state + move
+    try
+        checkbounds(e.rewards, next)
+        next
+    catch
+        state
+    end
 end
 
-getNextAction(q::QLearning, position::State, ϵ) = let
-  index = rand() < ϵ ? argmax(q.values[position,:]) : rand(1:Action.size)
-  Action(index)
-end
-
-function getNextLocation(q::QLearning, position::State, action::Action)
-  move = if action == up;    State(-1,0)
-     elseif action == right; State(0,1)
-     elseif action == down;  State(1,0)
-     elseif action == left;  State(0,-1)
-     end
-  next = position + move
-  try
-    checkbounds(q.values[:,:,1], next)
-    next
-  catch
-    position
-  end
-end
-
-function getShortestPath(q::QLearning, position::State)
-  path = [position]
-  while !isterminal(position, q)
-    action = getNextAction(q, position, 1.0)
-    position = getNextLocation(q, position, action)
-    push!(path, position)
+function get_shortest_path(a::Agent, e::Environment, state::State)
+  path = [state]
+  while !isterminal(state, e)
+    action = get_next_action(a, e, state, 1.0)
+    state = get_next_state(e, state, action)
+    push!(path, state)
   end
   path
 end
 
-# Initialise the QLearning
-q = readdlm("rewards.csv", ',') |> QLearning
+# Initialise the environment and agent
+env = Environment(readdlm("rewards.csv", ','), 4)
+agent = Agent(env)
 
 # Training part
 ϵ = 0.7
-for _ in 1:5_000
-  position = getStartingLocation(q)
-  while !isterminal(position, q)
-    action = getNextAction(q, position, ϵ)
-    nextPostion = getNextLocation(q, position, action)
-    update_qvalues!(q, position, nextPostion, action)
-    position = nextPostion
+episodes = 5_000
+for _ in 1:episodes
+  state = get_starting_state(env)
+  while !isterminal(state, env)
+    action = get_next_action(agent, env, state, ϵ)
+    next_state = get_next_state(env, state, action)
+    update_qvalues!(agent, env, state, next_state, action)
+    state = next_state
   end
 end
 
-# Tests
-startingPoint = [State(4,10), State(6,1), State(10,6), State(6,3)]
+# # Tests
+starting_states = [State(4,10), State(6,1), State(10,6), State(6,3)]
 
-foreach(startingPoint) do point
-  path = getShortestPath(q, point)
-  grid = zeros(Int8, size(q.rewards))
+foreach(starting_states) do state
+  path = get_shortest_path(agent, env, state)
+  grid = zeros(Int8, size(env.rewards))
   grid[path] .= 99
   display(grid)
   println('\n')
